4844a1d06de02011136ec7a01b5cb2f01c5eb415
Jeff Gillis <jgillis@optoro.com>
2015-01-16 13:38:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DummyRepo::Application.config.secret_token = '7ad09c42d9c5f9e34a8ad78c97fc79f4ffe97270beb9493246d0143526b1eb350a6497ed37e956d6ce13e18b5b9d8e3dab6d5fb6529705b4e91af51e866f0440'
